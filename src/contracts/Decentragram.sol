2cbd4b19b1450467b98c45efa34ac92f27d4a69d
Serik <csagymbek@gmail.com>
2021-10-27 00:54:01 -0400

    test cases added

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -4,12 +4,38 @@ contract Decentragram {
   string public name = "Decentragram";
 
   // store image 
+  uint public imageCount = 0;
   mapping (uint => Image) public images;
   struct Image {
     uint id;
     string hash;
     string description;
     uint tipAmount;
-    address payable author;
+    address  author;
   }
+
+  event ImageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address author
+  );
+
+  // create images 
+  function uploadImage(string memory _imgHash, string memory _description) public {
+    // requirements for arguments
+    require(bytes(_description).length > 0);
+    require(bytes(_imgHash).length > 0);
+    require(msg.sender != address(0x0));
+
+    // increment image id 
+    imageCount++;
+
+    // add image to contract 
+    images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+
+    // trigger an event 
+    emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+  } 
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 const Decentragram = artifacts.require("./Decentragram.sol");
 
 require("chai").use(require("chai-as-promised")).should();
@@ -21,6 +23,66 @@ contract("Decentragram", ([deployer, author, tipper]) => {
     it("has a name", async () => {
       const name = await decentragram.name();
       assert.equal(name, "Decentragram");
+    });
+  });
+
+  describe("images", async () => {
+    let result, imageCount;
+    const hash = "0x5A8B363DF475D9301f0482f36586a4Bc23b2F017";
+
+    before(async () => {
+      result = await decentragram.uploadImage(
+        hash,
+        "a double decker bus driving down a street",
+        { from: author }
+      );
+      imageCount = await decentragram.imageCount();
+    });
+
+    it("createss images", async () => {
+      // success
+      assert.equal(imageCount, 1);
+      const event = result.logs[0].args;
+
+      // console.log(event);
+
+      assert.equal(
+        event.id.toNumber(),
+        imageCount.toNumber(),
+        "Given ID is correct..."
+      );
+      assert.equal(event.hash, hash, "Given hash is correct...");
+      assert(
+        event.description,
+        "Image description",
+        "Given description is correct..."
+      );
+      assert.equal(event.tipAmount, 0, "Given tip amount correct...");
+      assert.equal(event.author, author, "Given author is correct...");
+
+      await decentragram.uploadImage("", "Image description", { from: author })
+        .should.be.rejected;
+
+      await decentragram.uploadImage("Image hash", "", { from: author }).should
+        .be.rejected;
+
+      // checking for struct
+      it("should list images", async () => {
+        const image = await decentragram.images(imageCount);
+        assert.equal(
+          image.id.toNumber(),
+          imageCount.toNumber(),
+          "Given ID is correct..."
+        );
+        assert.equal(image.hash, hash, "Given hash is correct...");
+        assert(
+          image.description,
+          "Image description",
+          "Given description is correct..."
+        );
+        assert.equal(image.tipAmount, 0, "Given tip amount correct...");
+        assert.equal(image.author, author, "Given author is correct...");
+      });
     });
   });
 });
